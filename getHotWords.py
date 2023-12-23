1d9673121f237d0fa9537c7e46728eda64a16718
RuoYan <cmu.liugelei@gmail.com>
2023-12-23 13:52:19 +0800

    fix:添加小写转换

--- getHotWords.py
+++ getHotWords.py
@@ -18,12 +18,15 @@ words = re.findall(r'\b\w+\b', data, flags=re.UNICODE)
 for word in words:
     word_count[word] = word_count.get(word, 0) + 1
 
+# 转化成小写
+words = [word.lower() for word in words]
+
 # 按照频率降序排序
 sorted_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
 
 # 将结果保存到hotWords.csv文件
 with open('hotWords.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    # writer.writerow(['Word', 'Frequency'])
+    writer.writerow(['Word', 'Frequency'])
     for word, frequency in sorted_words:
         writer.writerow([word, frequency])
