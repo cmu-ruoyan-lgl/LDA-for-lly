71b7a45deb207c2965907b17441525299a16d005
RuoYan <cmu.liugelei@gmail.com>
2023-12-23 13:54:27 +0800

    fix:修改LDA参数

--- LDA.py
+++ LDA.py
@@ -2,6 +2,8 @@ import pandas as pd
 import gensim
 from gensim import corpora
 
+# The LDA parameters are "α = 50 / k and £ = 0.1".
+
 # 1. 加载hotwords.csv文件
 data = pd.read_csv('hotwords.csv')
 
@@ -12,11 +14,16 @@ dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 # 3. 创建LDA模型
+num_topics = 10  # 设置主题数
+alpha = 50 / num_topics  # 设置α参数
+eta = 0.1  # 设置£参数
 lda_model = gensim.models.LdaModel(corpus=corpus,
                                    id2word=dictionary,
-                                   num_topics=10,   # 设置主题数
-                                   passes=10,       # 迭代次数
-                                   random_state=123) # 随机种子
+                                   num_topics=num_topics,
+                                   alpha=alpha,
+                                   eta=eta,
+                                   passes=10,
+                                   random_state=123)
 
 # 4. 可视化主题
 import pyLDAvis.gensim_models
