b19e2effbb90bcba3cfa068f9f3b3015af2d6a2c
RuoYan <cmu.liugelei@gmail.com>
2023-12-27 16:57:49 +0800

    tag 2.0

--- LDA.py
+++ LDA.py
@@ -15,7 +15,7 @@ dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 # 3. 创建LDA模型
-num_topics = 10  # 设置主题数
+num_topics = 7  # 设置主题数
 alpha = 50 / num_topics  # 设置α参数
 eta = 0.1  # 设置£参数
 lda_model = gensim.models.LdaModel(corpus=corpus,
@@ -31,6 +31,12 @@ import pyLDAvis.gensim_models
 vis = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary)
 print('LDA可视化结果')
 print(vis)
+print('LDA可视化结果存到xlsx中')
+pd.DataFrame(vis.topic_info).to_excel("TF-IDF-topic_info.xlsx", index=False)
+pd.DataFrame(vis.topic_coordinates).to_excel("TF-IDF-topic_coordinates.xlsx", index=False)
+pd.DataFrame(vis.token_table).to_excel("TF-IDF-token_table.xlsx", index=False)
+
+
 print('LDA可视化结果保存为html文件')
 pyLDAvis.display(vis)
 pyLDAvis.save_html(vis, 'lda_visualization.html')
@@ -40,12 +46,16 @@ print('LDA模型训练完成')
 tf_idf_vectorizer = TfidfVectorizer()
 tf_idf = tf_idf_vectorizer.fit_transform(pd.DataFrame(texts))
 
+# 将数据保存到TF-IDF.xlsx文件中
+# pd.DataFrame(texts).to_excel("TF-IDF.xlsx", index=False)
+
 print(pd.DataFrame(texts))
 # 特征词列表
 feature_names = tf_idf_vectorizer.get_feature_names_out()
 # 特征词 TF-IDF 矩阵
+print('开始构建TF-IDF矩阵')
 matrix = tf_idf.toarray()
 feature_names_df = pd.DataFrame(matrix,columns=feature_names)
-print('TF-IDF矩阵')
+print('TF-IDF矩阵构建结束')
 print(feature_names_df)
 feature_names_df
